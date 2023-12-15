587c2adffc2ec3b4985575d370d80775d80a5504
Yohannes Misganaw Assefa <yomifalsa@gmail.com>
2023-12-15 14:09:47 -0800

    advanced betty fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point, prints all possible different combinations of two digits.
+ * main - Entry point, prints all possible different combinations of 
+ * two digits.
  *
  * Return: Always 0 (Success)
  */
